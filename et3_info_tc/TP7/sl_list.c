865c5185c0208970f52010320a4b05d076bbf9cf
Lucas David <ldavid4@a-018818.ups.u-psud.fr>
2017-12-19 14:29:43 +0100

    TP7

--- et3_info_tc/TP7/sl_list.c
+++ et3_info_tc/TP7/sl_list.c
@@ -1,16 +1,38 @@
 #include "sl_list.h"
 
 sl_list_t* create_sl_list() {
-
-
+	sl_list_t* that = malloc(sizeof(sl_list_t));
+	that->first = NULL;
+	return that;
 }
 
-void delete_sl_list(sl_list_t* l);
+void delete_sl_list(sl_list_t* l) {
+	free(l);
+}
 
-int is_empty_sl_list(sl_list_t* l);
+int is_empty_sl_list(sl_list_t* l) {
+	if(l->first)
+		return 0;
+	else
+		return 1;
+}
 
-void push_front_sl_list(sl_list_t* l);
+void push_front_sl_list(sl_list_t* l, sl_cell_t* c) {
+	if(l->first)
+		l->c->next = l->first;
+	l->first = c;
+}
 
-void push_back_sl_list(sl_list_t* l);
+void push_back_sl_list(sl_list_t* l, sl_cell_t* c) {
+	if(l->first) {
+		sl_cell_t* current_cell = l->first;
+		while(current_cell->next)
+			current_cell = current_cell->next;
+		current_cell->next = c;
+	}
+	l->first = c;
+}
 
-void find_sl_list(sl_list_t* l);
+sl_cell_t* find_sl_list(sl_list_t* l) {
+	
+}
