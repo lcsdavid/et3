0f8834ceec7e3669816f0f9ed58b672ae723cd30
lcsdavid <david-lucas-69@hotmail.fr>
2018-04-11 14:12:28 +0200

    Up TP6 ok

--- S6/Concepts objet et Java/TP6/src/Culotte.java
+++ S6/Concepts objet et Java/TP6/src/Culotte.java
@@ -1,28 +1,47 @@
-import java.util.Hashtable;
+
+import java.util.*;
 
 public class Culotte {
-    private Hashtable<String, Integer> m_hash;
+    static final public Comparator<Map.Entry<String, Long>> ENTRY_COMPARATOR = new Comparator<Map.Entry<String, Long>>() {
+        @Override
+        public int compare(Map.Entry<String, Long> o1, Map.Entry<String, Long> o2) {
+            return o1.getValue() < o2.getValue() ? 1 : o1.getValue() > o2.getValue() ? -1 : 0;
+        }
+    };
+
+    private Hashtable<String, Long> table;
 
     public Culotte() {
-        m_hash = new Hashtable<>();
+        table = new Hashtable<>();
     }
 
     public void analyser(String texte) {
         String[] ss = texte.split(" ");
         for (String s: ss) {
             s = s.replaceAll("[^A-Za-z0-9_'-]", "").toLowerCase();
-            if (!s.isEmpty()) {
-                m_hash.computeIfPresent(s, (k, v) -> m_hash.replace(k, v++));
-                m_hash.putIfAbsent(s, 1);
-            }
+
+            if (!s.isEmpty())
+                if (table.containsKey(s))
+                    table.replace(s, table.get(s) + 1);
+                else
+                    table.put(s, (long)1);
         }
     }
 
-    public int fréquenceMot(String mot) {
+    public long fréquenceMot(String mot) {
         assert mot != null;
-        if(m_hash.containsKey(mot))
-            return m_hash.get(mot);
+        if(table.containsKey(mot))
+            return table.get(mot);
         return 0;
     }
 
+    public Set<Map.Entry<String, Long>> entrySet() {
+        return table.entrySet();
+    }
+
+    public List ordenedList() {
+        List list = new ArrayList(entrySet());
+        list.sort(ENTRY_COMPARATOR);
+        return list;
+    }
 }
